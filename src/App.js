7a5c998c00ed506b243df895fe2a51c1050683d4
IceMupppet <feras alkilani>
2022-11-02 02:25:16 +0300

    Feras Alkilani

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Feras Alkilani
         </a>
       </header>
     </div>
